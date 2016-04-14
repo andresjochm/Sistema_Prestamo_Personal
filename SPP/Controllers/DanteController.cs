c6c60c5a80a13fb95de3f165fb5865e1ad23c1b6
dante ccarhuas <ccarhuas.dante@gmail.com>
2016-04-14 15:51:35 -0500

    sesd

---
+++ SPP/Controllers/DanteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SPP.Controllers
+{
+    public class DanteController : Controller
+    {
+        // GET: Dante
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
